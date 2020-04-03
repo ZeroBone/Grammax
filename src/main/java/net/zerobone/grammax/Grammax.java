80500bfbb120bccc1dee3896c19658ed113b39db
ZeroBone <zerobone21@gmail.com>
2020-04-03 11:32:13 +0200

    Initial commit

---
+++ src/main/java/net/zerobone/grammax/Grammax.java
@@ -0,0 +1,11 @@
+package net.zerobone.grammax;
+
+public class Grammax {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello grammax!!!!");
+
+    }
+
+}
---
+++ src/test/java/GrammaxTest.java
@@ -0,0 +1,13 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class GrammaxTest {
+
+    @Test
+    public void testSomething() {
+
+        Assert.assertEquals(1, 1);
+
+    }
+
+}
