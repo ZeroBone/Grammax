bbf34cbd1038537fe9bb4f6d2d4f47db5102c9bc
ZeroBone <zerobone21@gmail.com>
2020-04-03 11:56:13 +0200

    Build trigger.

--- src/main/java/net/zerobone/grammax/Grammax.java
+++ src/main/java/net/zerobone/grammax/Grammax.java
@@ -4,7 +4,7 @@ public class Grammax {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello grammax!!!!");
+        System.out.println("Hello grammax!!!! Build trigger.");
 
     }
 
--- src/test/java/net/zerobone/grammax/GrammaxTest.java
+++ src/test/java/net/zerobone/grammax/GrammaxTest.java
@@ -1,3 +1,5 @@
+package net.zerobone.grammax;
+
 import org.junit.Assert;
 import org.junit.Test;
 
