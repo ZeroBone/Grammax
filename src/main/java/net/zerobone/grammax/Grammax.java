91b555dc6c291efc33cc1c52a017c576e6cbf9be
Alexander Mayorov <zerobone21@gmail.com>
2024-12-27 22:20:19 +0100

    Changed version to 1.1.1

--- src/main/java/net/zerobone/grammax/Grammax.java
+++ src/main/java/net/zerobone/grammax/Grammax.java
@@ -20,7 +20,7 @@ import java.util.*;
 
 public class Grammax {
 
-    public static final String VERSION = "1.1.0";
+    public static final String VERSION = "1.1.1";
 
     private final GrammaxContext context;
 
